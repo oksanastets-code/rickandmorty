b227866d766d9381d0c18b5f6bcd6d31b10df4ce
Oksana Stets <oksanastets73@gmail.com>
2023-03-17 23:02:34 +0200

    Update CharactersList.jsx

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -10,16 +10,15 @@ export default function CharactersList({ characters }) {
       {characters.sort(sortByName).map(character => (
         <Item key={character.id}>
           <Link to={`/${character.id}`} state={{ from: location }}>
-            <div>
-              <ImgWrapper p={character.image}
+            <CardWrapper>
+              <ImgWrapper style={{backgroundImage: `url(${character.image})`}}
               >
-                {/* <img src={character.image} alt={character.name} /> */}
               </ImgWrapper>
               <TextWrapper>
-                <p>{character.name}</p>
-              <p>Specie {character.species}</p>
+                <Name>{character.name}</Name>
+              <Specie>Specie {character.species}</Specie>
               </TextWrapper>              
-            </div>
+            </CardWrapper>
           </Link>
         </Item>
       ))}
@@ -42,12 +41,30 @@ const Item = styled.li`
     margin-right: 0;
   }
 `
+const CardWrapper = styled.div`
+  border-radius: 3px;
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.2), 0px 3px 3px rgba(0, 0, 0, 0.12),
+      0px 2px 4px rgba(0, 0, 0, 0.14);
+`
 const ImgWrapper = styled.div`
   height: 168px;
-  background-image: url('https://rickandmortyapi.com/api/character/avatar/1.jpeg');
   background-size: cover;
   background-position-y: -30px;
 `
 const TextWrapper = styled.div`
   padding: 12px 16px;
+`
+const Name = styled.p`
+  color: rgba(0, 0, 0, 0.87);
+  font-weight: 500;
+font-size: 20px;
+line-height: 30px;
+letter-spacing: 0.15px;
+`
+const Specie = styled.p`
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 400;
+font-size: 14px;
+line-height: 21px;
+letter-spacing: 0.25px;
 `

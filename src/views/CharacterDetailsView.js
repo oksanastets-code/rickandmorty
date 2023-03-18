c09ba4b45dc721131e3b753253f39e9f719263f5
Oksana Stets <oksanastets73@gmail.com>
2023-03-18 16:59:05 +0200

    Update CharacterDetailsView.js

--- src/views/CharacterDetailsView.js
+++ src/views/CharacterDetailsView.js
@@ -21,7 +21,7 @@ export default function CharacterDetailsView({ onClick }) {
         <CardWrapper>
           <CharacterImg src={character.image} alt="" />
           <Name>{character.name}</Name>
-          <Title>Infomations</Title>
+          <Title>Information</Title>
           <DataWrapper>
             <ItemWrapper>
               <DataTitle>Specie</DataTitle>
@@ -55,50 +55,75 @@ export const characterByIdLoader = async ({ params }) => {
 };
 
 const Container = styled.div`
-width: 100%;
-padding: 20px 50px 142px;
-`
+  width: 100%;
+  padding: 24px;
+
+  @media screen and (min-width: 481px) {
+    padding: 20px 50px 142px;
+  }
+`;
 const CardWrapper = styled.div`
-  width: 1020px;
-  margin: 32px auto 0;
+  width: 312px;
+  margin: 77px auto 0;
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media screen and (min-width: 481px) {
+    width: 1020px;
+    margin: 32px auto 0;
+  }
 `;
 const CharacterImg = styled.img`
   display: block;
-  width: 310px;
-  height: 310px;
+  width: 160px;
+  height: 160px;
   border-radius: 50%;
   border: 5px solid #f2f2f7;
+  @media screen and (min-width: 481px) {
+    width: 310px;
+    height: 310px;
+  }
 `;
 const Name = styled.p`
-  margin-top: 16px;
+  margin-top: 34px;
   color: #081f32;
-  font-size: 48px;
-  line-height: 56px;
+  font-size: 30px;
+  line-height: 38px;
   text-align: center;
+  @media screen and (min-width: 481px) {
+    margin-top: 16px;
+    font-size: 48px;
+    line-height: 56px;
+  }
 `;
 const Title = styled.p`
-  margin-top: 48px;
+  margin-top: 35px;
+  align-self: start;
   color: #8e8e93;
   font-weight: 500;
   font-size: 20px;
   line-height: 24px;
   letter-spacing: 0.15px;
+  @media screen and (min-width: 481px) {
+    margin-top: 48px;
+    align-self: center;
+  }
 `;
 const DataWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 412px;
-  margin-top: 48px;
+  width: 312px;
+  margin-top: 16px;
+  @media screen and (min-width: 481px) {
+   width: 412px;
+   margin-top: 48px;
+  }
 `;
 const ItemWrapper = styled.div`
   display: flex;
   flex-direction: column;
   border-bottom: 1px solid #ededed;
   width: 100%;
-  /* padding-top: 50px; */
   padding: 8px 16px 12px 16px;
 `;
 const DataTitle = styled.p`
